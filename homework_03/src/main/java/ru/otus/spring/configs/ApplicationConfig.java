d1f4b469a8095f24d91f90c9a528e5ee4926892b
Sergey Yazynin <sergey.yazynin@akvelon.com>
2023-05-27 00:54:30 +0300

    Fix comments v2

--- homework_03/src/main/java/ru/otus/spring/configs/ApplicationConfig.java
+++ homework_03/src/main/java/ru/otus/spring/configs/ApplicationConfig.java
@@ -1,11 +1,14 @@
 package ru.otus.spring.configs;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import ru.otus.spring.dao.TaskDao;
 import ru.otus.spring.dao.TaskDaoImpl;
+import ru.otus.spring.services.MessageService;
+import ru.otus.spring.services.MessageServiceImpl;
 import ru.otus.spring.services.TaskConverter;
 
 @Configuration
@@ -21,5 +24,10 @@ public class ApplicationConfig {
   @Bean
   public TaskDao taskDao(TaskConverter taskConverter) {
     return new TaskDaoImpl(new ClassPathResource(appProps.getFileName()), taskConverter);
+  }
+
+  @Bean
+  public MessageService messageService(MessageSource messageSource) {
+    return new MessageServiceImpl(messageSource, appProps.getLocale());
   }
 }
--- homework_03/src/main/java/ru/otus/spring/services/MessageServiceImpl.java
+++ homework_03/src/main/java/ru/otus/spring/services/MessageServiceImpl.java
@@ -2,29 +2,28 @@ package ru.otus.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Service;
-import ru.otus.spring.configs.AppProps;
 
-@Service
+import java.util.Locale;
+
 public class MessageServiceImpl implements MessageService {
 
   private final MessageSource messageSource;
 
-  private final AppProps props;
+  private final Locale locale;
 
   @Autowired
-  public MessageServiceImpl(MessageSource messageSource, AppProps props) {
+  public MessageServiceImpl(MessageSource messageSource, Locale locale) {
     this.messageSource = messageSource;
-    this.props = props;
+    this.locale = locale;
   }
 
   @Override
   public String getMessage(String msgCode) {
-    return messageSource.getMessage(msgCode, null, props.getLocale());
+    return messageSource.getMessage(msgCode, null, locale);
   }
 
   @Override
   public String getMessage(String msgCode, String... strings) {
-    return messageSource.getMessage(msgCode, strings, props.getLocale());
+    return messageSource.getMessage(msgCode, strings, locale);
   }
 }
