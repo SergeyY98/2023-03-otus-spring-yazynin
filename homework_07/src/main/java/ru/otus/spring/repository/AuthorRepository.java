eb12c66e3a021b62afc2987e7ef63185416a32e2
Sergey Yazynin <sergey.yazynin@akvelon.com>
2023-07-26 01:05:44 +0300

    fix

--- homework_07/src/main/java/ru/otus/spring/repository/AuthorRepository.java
+++ homework_07/src/main/java/ru/otus/spring/repository/AuthorRepository.java
@@ -3,17 +3,5 @@ package ru.otus.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.spring.domain.Author;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-  long count();
-
-  Author save(Author book);
-
-  Optional<Author> findById(long id);
-
-  List<Author> findAll();
-
-  void deleteById(long id);
 }
--- homework_07/src/main/java/ru/otus/spring/repository/CommentRepository.java
+++ homework_07/src/main/java/ru/otus/spring/repository/CommentRepository.java
@@ -1,25 +1,15 @@
 package ru.otus.spring.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.otus.spring.domain.Comment;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-  long count();
 
-  Comment save(Comment comment);
-
-  Optional<Comment> findById(long id);
-
-  @EntityGraph(attributePaths = "book")
   @Query("select c from Comment c where exists ( " +
       "select 1 from Book b where c.book=b and b.id = :book_id)")
   List<Comment> findAllByBookId(@Param("book_id") long id);
-
-  void deleteById(long id);
 }
